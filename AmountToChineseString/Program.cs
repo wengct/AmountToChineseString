c854aaf1b7afa7da7c458ee3db8b1a3af5c47087
Chenting.Weng <wengct13@gmail.com>
2020-03-28 12:17:01 +0800

    update

--- AmountToChineseString/Program.cs
+++ AmountToChineseString/Program.cs
@@ -11,18 +11,18 @@ namespace AmountToChineseString
         public static void Main(string[] args)
         {
             int n = 1033;
-            Console.WriteLine(AmountToString(n, PaddingZeroUnit.佰萬));
+            Console.WriteLine(AmountToString(n, PaddingZeroUnit.NoPadding));
+            Console.WriteLine(AmountToString(n, PaddingZeroUnit.HundredMillion));
         }
 
         public static string AmountToString(long amount, PaddingZeroUnit paddingZeroUnit)
         {
-            string result = string.Empty;
             int length = amount.ToString().Length - 1;
             if (amount <= 0)
             {
                 throw new ArgumentOutOfRangeException("amount", "數值必需為正整數");
             }
-            else if ((int)paddingZeroUnit != 0 && length > (int)paddingZeroUnit)
+            else if (paddingZeroUnit != 0 && length > (int)paddingZeroUnit)
             {
                 throw new ArgumentOutOfRangeException("paddingZeroUnit", "限定最高單位低於傳入金額");
             }
@@ -33,31 +33,80 @@ namespace AmountToChineseString
 
             string[] numberTexts = { "零", "壹", "貳", "參", "肆", "伍", "陸", "柒", "捌", "玖" };
             string[] typeMappings = { "", "拾", "佰", "仟", "萬", "拾", "佰", "仟", "億", "拾", "佰", "仟", "兆" };
-            int loopTimes = (int)paddingZeroUnit == 0 ? length : (int)paddingZeroUnit;
+            int loopTimes = paddingZeroUnit == 0 ? length : (int)paddingZeroUnit;
+            long pow10, tempNum;
+            List<string> result = new List<string>();
             for (int i = loopTimes; i >= 0; i--)
             {
-                long temp = amount / Convert.ToInt64(Math.Pow(10, i));
-                amount -= temp * Convert.ToInt64(Math.Pow(10, i));
-                result += " " + numberTexts[temp] + " " + typeMappings[i];
+                pow10 = Convert.ToInt64(Math.Pow(10, i));
+                tempNum = amount / pow10;
+                amount -= tempNum * pow10;
+                result.Add(numberTexts[tempNum]);
+                if (typeMappings[i] != "")
+                {
+                    result.Add(typeMappings[i]);
+                }
             }
-            return result;
+            return string.Join(" ", result);
         }
 
+        /// <summary>
+        /// 顯示最高單位，不足補零
+        /// </summary>
         public enum PaddingZeroUnit
         {
-            開頭不補零 = 0,
-            拾 = 1,
-            佰,
-            仟,
-            萬,
-            拾萬,
-            佰萬,
-            仟萬,
-            億,
-            拾億,
-            佰億,
-            仟億,
-            兆
+            /// <summary>
+            /// 開頭不補零
+            /// </summary>
+            NoPadding = 0,
+            /// <summary>
+            /// 拾
+            /// </summary>
+            Ten,
+            /// <summary>
+            /// 佰
+            /// </summary>
+            Hundred,
+            /// <summary>
+            /// 仟
+            /// </summary>
+            Thousand,
+            /// <summary>
+            /// 萬
+            /// </summary>
+            TenThousand,
+            /// <summary>
+            /// 拾萬
+            /// </summary>
+            HundredThousand,
+            /// <summary>
+            /// 佰萬
+            /// </summary>
+            Million,
+            /// <summary>
+            /// 佰萬
+            /// </summary>
+            TenMillion,
+            /// <summary>
+            /// 億
+            /// </summary>
+            HundredMillion,
+            /// <summary>
+            /// 拾億
+            /// </summary>
+            Billion,
+            /// <summary>
+            /// 佰億
+            /// </summary>
+            TenBillion,
+            /// <summary>
+            /// 仟億
+            /// </summary>
+            HundredBillion,
+            /// <summary>
+            /// 兆
+            /// </summary>
+            Trillion
         }
     }
 }
